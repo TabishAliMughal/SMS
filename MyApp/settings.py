a5522f035a8072410e58d360dc6a7f617c5ebe2d
Tabish Ali Mughal <56387446+TabishAliMughal@users.noreply.github.com>
2023-07-19 10:37:21 +0500

    Update settings.py

--- MyApp/settings.py
+++ MyApp/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ovy6ph=k&ml+dqs!0ooz0orceqg-x58bnncg)4c7_581mgulx#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*','*.*']
 
